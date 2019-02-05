7a64e19707b7916fa25e594c6e85a5418c179d9f
Heiner Bunjes <heiner@bunjes.net>
2019-02-05 10:41:39 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -17,19 +17,7 @@ echo "#### build.sh has started."
 
 cd ~/docker-autossh
 
-
-# 4: elk host -> elk container
-# docker-compose.yml
-# "514:5514/udp"   syslog   -> logstash
-# "515:5515"       syslog   -> logstash
-# "5000:5000"      other    -> logstash
-# "5001:5001/udp"  logspout -> logstash
-# "5601:5601"      kibana
-# "9001:9000"      cerebro
-# "9200:9200"      data     -> elasticsearch
-
-
-# 3: rpi container -> elk host
+# 4: rpi container -> elk host
 HOST_FORWARDS=""
 HOST_FORWARDS+="*:514:127.0.0.1:514"
 HOST_FORWARDS+=","
@@ -48,6 +36,17 @@ HOST_FORWARDS+=","
 HOST_FORWARDS+="*:9200:127.0.0.1:9200"
 
 echo "HOST_FORWARDS = '$HOST_FORWARDS'"
+
+# 5: elk host -> elk container
+# docker-compose.yml
+# "514:5514/udp"   syslog   -> logstash
+# "515:5515"       syslog   -> logstash
+# "5000:5000"      other    -> logstash
+# "5001:5001/udp"  logspout -> logstash
+# "5601:5601"      kibana
+# "9001:9000"      cerebro
+# "9200:9200"      data     -> elasticsearch
+
 
 docker build                                         \
 --build-arg HOST_NAME="elk.bunjes.net"               \
